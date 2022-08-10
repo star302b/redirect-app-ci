4067236d700e6d8c744d111829bf1b59396bb5e5
Denys Miahkyi <denismagkij@Denyss-MacBook-Pro.local>
2022-08-10 18:56:51 +0200

    rollabck

--- app/Http/Controllers/FunnelRedirect.php
+++ app/Http/Controllers/FunnelRedirect.php
@@ -18,6 +18,6 @@ class FunnelRedirect extends Controller
 
         $redirect_url = $response->body();
 
-        return $redirect_url;//redirect($redirect_url, 301);
+        return redirect($redirect_url, 301);
     }
 }
